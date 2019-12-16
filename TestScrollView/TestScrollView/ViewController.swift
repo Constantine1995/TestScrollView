8e2a24038019c8332579ea06dae9f06c48f7e48b
Константин Лихачёв <constantine.lord.darkness@gmail.com>
2019-12-16 18:13:30 +0200

    initial

---
+++ TestScrollView/TestScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestScrollView
+//
+//  Created by Constantine Likhachov on 16.12.2019.
+//  Copyright © 2019 Constantine Likhachov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
